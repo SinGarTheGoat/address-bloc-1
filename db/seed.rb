c59c9630c28f43fd381d871d4b8e34ee114abdff
Forrest Stone <iloveburningman@gmail.com>
2017-09-15 18:12:43 -0700

    CH 2 follow along done

--- db/seed.rb
+++ db/seed.rb
@@ -1,5 +1,5 @@
-require_relative '../../address-bloc-1/models/address_book'
-require_relative '../../address-bloc-1/models/entry'
+require_relative '../models/address_book'
+require_relative '../models/entry'
 require 'bloc_record'
 
 BlocRecord.connect_to('db/address_bloc.sqlite')
